f52e49ab0bd022fbcb82a15d50e6f9ab7ff6e317
Enias Cailliau <hello@enias.io>
2022-08-09 10:59:04 +0200

    Add auth

--- src/auth.py
+++ src/auth.py
@@ -1,5 +1,7 @@
+import streamlit as st
+
+
 def check_auth() -> None:
-    pass
-    # password = st.sidebar.text_input(label="Password", type="password")
-    # if password != "LetTheMagicBegin":
-    #     st.stop()
+    password = st.sidebar.text_input(label="Password", type="password")
+    if password != "LetTheMagicBegin":
+        st.stop()
